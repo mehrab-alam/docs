7bf2ec78139c61ff63b9e5243738634d9a799d78
Mehrab <mehrab2000alam@gmail.com>
2025-08-09 02:16:17 +0000

    added custom js file

---
+++ chat-embed.js
@@ -0,0 +1,6 @@
+const script = document.createElement("script");
+script.src = "https://torqbit-dev.b-cdn.net/static/js/chat-embed.dev.js";
+script.defer = true;
+script.dataset.agentId = "cme2shyaa0013pa6egpuvufkv";
+script.dataset.position = "bottom-right";
+document.head.appendChild(script);
