a469d587f0e9ea6ec4aa2b3a46c36403c8ab817b
Mehrab <mehrab2000alam@gmail.com>
2025-08-09 08:01:32 +0530

    Documentation edits made through Mintlify web editor

--- chat-embed.js
+++ chat-embed.js
@@ -1,6 +1,11 @@
-const script = document.createElement("script");
-script.src = "https://torqbit-dev.b-cdn.net/static/js/chat-embed.dev.js";
-script.defer = true;
-script.dataset.agentId = "cme2shyaa0013pa6egpuvufkv";
-script.dataset.position = "bottom-right";
-document.head.appendChild(script);
+(function () {
+  const script = document.createElement("script");
+  script.src = "https://torqbit-dev.b-cdn.net/static/js/chat-embed.dev.js";
+  script.defer = true;
+
+  // Preserve exact attribute casing
+  script.setAttribute("data-agentId", "cme2shyaa0013pa6egpuvufkv");
+  script.setAttribute("data-position", "bottom-right");
+
+  document.head.appendChild(script);
+})();
