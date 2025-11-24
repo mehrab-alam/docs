87cfc6f4405c182e0661056f0c6df612c8da5079
Mehrab <mehrab2000alam@gmail.com>
2025-11-24 13:07:37 +0530

    Documentation edits made through Mintlify web editor

--- chat-embed.js
+++ chat-embed.js
@@ -1,10 +1,10 @@
 (function () {
   const script = document.createElement("script");
-  script.src = "https://cdn.torqbit.com/static/js/chat-embed.local.js?v=" + Date.now();
+  script.src = "https://cdn.torqbit.com/static/js/chat-embed.dev.js?v=" + Date.now();
   script.defer = true;
 
   // Preserve exact attribute casing
-  script.setAttribute("data-agentId", "cmgt0emlr0016cmib54xnubc3");
+  script.setAttribute("data-agentId", "cmi73vgbg000gp2gpm7uqy7x9");
   script.setAttribute("data-position", "bottom-right");
   document.head.appendChild(script);
 })();
