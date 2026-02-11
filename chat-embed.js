b0f1ec1c85eb4c3ee410ef0f234439dfd12f7dae
Mehrab <mehrab2000alam@gmail.com>
2026-02-11 16:41:56 +0530

    Update chat-embed.js

--- chat-embed.js
+++ chat-embed.js
@@ -1,10 +1,10 @@
 (function () {
   const script = document.createElement("script");
-  script.src = "https://cdn.torqbit.com/static/js/toq.local.js?v=" + Date.now();
+  script.src = "https://cdn.torqbit.com/static/js/chat-embed.prod.js?v=" + Date.now();
   script.defer = true;
 
   // Preserve exact attribute casing
-  script.setAttribute("data-agentId", "cml535w880014k9ffz8odvajt");
+  script.setAttribute("data-agentId", "cmlg5hfir000kywyycyh65u0e");
   script.setAttribute("data-position", "bottom-right");
 
   document.head.appendChild(script);
